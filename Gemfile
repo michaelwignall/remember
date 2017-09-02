0367c638d4b40a21d728acea51f3286f52738533
Mike Wignall <michael@born-digital.co.uk>
2017-09-02 12:58:25 +0100

    Add code coverage metrics

--- Gemfile
+++ Gemfile
@@ -68,3 +68,7 @@ group :development do
   gem "binding_of_caller"
 end
 
+group :test do
+  # Code coverage
+  gem 'simplecov', :require => false
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
 ENV['RAILS_ENV'] ||= 'test'
+
+# Simple coverage
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
