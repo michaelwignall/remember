ad635cdee9a941a1919456c8a456bc20fbdac713
Mike Wignall <michael@born-digital.co.uk>
2018-03-23 23:19:27 +0000

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170829213512) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.string "slug"
